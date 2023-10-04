d7e65d00bc9b3885d985067153661fd37d5e5393
Alex Garrett-Smith <alex@codecourse.com>
2023-10-04 14:37:44 +0100

    Episode 03

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,9 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
 
-    @vite(['resources/css/app.css', 'resources/app/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    {{ $slot }}
+    <div class="max-w-2xl mx-auto mt-32">
+        {{ $slot }}
+    </div>
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,18 +1,29 @@
 <?php
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 
-state(['counter' => 0]);
+state('url');
 
-$increment = function () {
-    $this->counter++;
+rules(['url' => 'required|url']);
+
+$submit = function () {
+    $this->validate();
 }
 ?>
 
 <x-app-layout>
     @volt
-        <div>
-            {{ $counter }}
-            <button wire:click="increment">Increment</button>
-        </div>
+        <form wire:submit="submit">
+            <input type="text" id="url" wire:model="url" class="w-full rounded-lg border-slate-300 text-slate-800 h-14 px-5 text-lg placeholder:text-slate-400 focus:ring-2 focus:ring-blue-500" placeholder="e.g. https://google.com">
+
+            <div class="flex items-baseline space-x-4">
+                <button type="submit" class="bg-blue-500 text-blue-50 rounded-lg px-10 mt-4 h-11 font-medium">Get short URL</button>
+
+                @error('url')
+                    <div>
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+        </form>
     @endvolt
 </x-app-layout>
